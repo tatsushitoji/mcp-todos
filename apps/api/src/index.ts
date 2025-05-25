b29f0ff03247ae7400df09e1c4f8743f6fdf4dce
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2025-05-26 00:48:51 +0900

    chore(api): add prisma into api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,15 +1,25 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { PrismaClient } from "../generated/prisma/index.js";
 
-const app = new Hono()
+const app = new Hono();
+const prisma = new PrismaClient();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/todos", async (c) => {
+  const todos = await prisma.todo.findMany();
+  return c.json(todos);
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 8787,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
